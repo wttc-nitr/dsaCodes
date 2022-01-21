fdfb3aaf0fda6b531c6060e01895d69bab043ba2
Animesh Kumar <kumaranimesh097@gmail.com>
2022-01-21 19:39:56 +0530

    add a file

---
+++ linkedList.cpp
@@ -0,0 +1,29 @@
+/*
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+*/
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define Ft first
+#define Sd second
+
+
+
+void solve(void){
+    
+}
+
+int main(void){
+     ios::sync_with_stdio(0); cin.tie(0);
+     
+    int test_cases = 1; 
+    while (test_cases-- > 0) {
+   	 solve();
+    }
+
+#ifndef ONLINE_JUDGE
+    cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+#endif
+    return 0;
+}
