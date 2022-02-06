e52ebed51a0800dc9c4b30df8a66c7134e2dc6e4
Animesh Kumar <kumaranimesh097@gmail.com>
2022-02-06 09:41:37 +0530

    stack using linkedlist added

---
+++ stackUsingLinkedList.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct Node {
+    int val;
+    struct Node *next;
+};
+
+typedef struct Node Node;
+
+void printList(Node *head){
+    while (head){
+        printf("%d ", head->val);
+        head = head->next;
+    }
+
+    printf("\n");
+}
+
+Node *head = NULL;
+
+bool isEmpty(void){
+    if (!head)
+        return true;
+    return false;
+}
+
+void Push(int x){
+    Node *new_node = (Node*)malloc(sizeof(int));
+    new_node->val = x;
+    new_node->next = NULL;
+
+    if (isEmpty())
+        head = new_node;
+    else {
+        new_node->next = head;
+        head = new_node;
+    }
+}
+
+void Pop(void){
+    if (isEmpty()){
+        printf("stack is already empty.\n");
+        return;
+    }
+
+    Node *temp = head;
+    head = head->next;
+    free(temp);
+}
+
+int Top(void){
+    if (isEmpty())
+        return -1;
+
+    return head->val;
+}
+
+int main(void){
+    for (int i=1; i <= 10; i++)
+        Push(i);
+
+    printList(head);
+
+    Pop();
+
+    printf("%d\n", Top());
+
+    return 0;
+}
