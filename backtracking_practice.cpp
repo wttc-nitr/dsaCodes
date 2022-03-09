c541a6635db33e7961ca63b3b0310cf316da4877
Animesh Kumar <kumaranimesh097@gmail.com>
2022-03-10 00:16:44 +0530

    add insert_recursively

--- backtracking_practice.cpp
+++ backtracking_practice.cpp
@@ -15,6 +15,21 @@ void Print(int n) {
     cout << n << ' '; 
 }
 
+vector<int> insert_recusively(vector<int>&arr, int x) {
+	if (arr.empty() || arr.back() <= x) {
+		arr.push_back(x);
+		return arr;
+	}
+
+	// if (!arr.empty() && arr.back() > x) {
+		int last = arr.back();
+		arr.pop_back();
+		auto brr = insert_recusively(arr, x);
+		brr.push_back(last);
+		return brr;
+	// }
+}
+
 int fact(int n) {
 	if (n == 1) {
 		return 1;
@@ -25,7 +40,11 @@ int fact(int n) {
 int main(void){
      ios::sync_with_stdio(0); cin.tie(0);
      
-    cout << fact(4);
+    vector<int> v{1, 2, 3, 4};
+    auto k = insert_recusively(v, 0);
+
+    for (auto i : k)
+    	cout << i << ' ';
 
 #ifndef ONLINE_JUDGE
     cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
